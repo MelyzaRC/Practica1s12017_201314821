6b6da208f19c07d33bbe78d8840c33ec76d896a9
MelyzaRC <mlzdrg792@gmail.com>
2017-02-14 01:10:56 -0600

    1402
    1402

---
+++ Practica1Estructuras/src/practica1estructuras/Practica1Estructuras.java
@@ -0,0 +1,13 @@
+
+package practica1estructuras;
+
+
+public class Practica1Estructuras {
+
+   
+    public static void main(String[] args) {
+      MainForm m = new MainForm();
+              m.setVisible(true);
+    }
+    
+}
